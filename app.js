e09cf6a19a92c0e9a627463ad378a30f5caf6bae
Mark <markherrington5@gmail.com>
2019-01-25 19:27:09 -0500

    did some stuff, probably wrong

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-app.js
+$('body').html(`<button><h2 id='viewbutton'>View</h2></button>`);
+$('body').append(`<button><h2 id='addbutton'>Add</h2></button>`);
+$('body').append(`<button><h2 id='verifybutton'>Verify</h2></button>`);
+
+showEmployees = function (employeeList) {
+    $('body').html(``);
+    $('body').html(`<button><h2 id='viewbutton'>View</h2></button>`);
+    $('body').append(`<button><h2 id='addbutton'>Add</h2></button>`);
+    $('body').append(`<button><h2 id='verifybutton'>Verify</h2></button>`);
+    for (i = 0; i < employeeList.length; i++)
+        $('body').append(`<div>${employeeList[i]}</div>`)
+    }
+
+addEmployees = function () {
+    $('body').html(``);
+    $('body').html(`<button><h2 id='viewbutton'>View</h2></button>`);
+    $('body').append(`<button><h2 id='addbutton'>Add</h2></button>`);
+    $('body').append(`<button><h2 id='verifybutton'>Verify</h2></button>`);
+    $('body').append(`<form id='nameform'>Name</form>`);
+    $('body').append(`<form id='numberform'>Office Number</form>`);
+    $('body').append(`<form id='phoneform'>Phone Number</form>`);
+}
+
+verifyEmployees = function () {
+    $('body').html(``);
+    $('body').html(`<button><h2 id='viewbutton'>View</h2></button>`);
+    $('body').append(`<button><h2 id='addbutton'>Add</h2></button>`);
+    $('body').append(`<button><h2 id='verifybutton'>Verify</h2></button>`);
+    
+}
+
+$('#viewbutton').on('click', showEmployees);
+$('#addbutton').on('click', addEmployees);
+$('#verifybutton').on('click', verifyEmployees);
