792b8f1178fc6e0878e6947f4976c07b29372e8c
Mark <markherrington5@gmail.com>
2019-01-25 21:33:23 -0500

    changed functions to the ones from the video, don't understand why '

--- app.js
+++ app.js
@@ -2,32 +2,25 @@ $('body').html(`<button><h2 id='viewbutton'>View</h2></button>`);
 $('body').append(`<button><h2 id='addbutton'>Add</h2></button>`);
 $('body').append(`<button><h2 id='verifybutton'>Verify</h2></button>`);
 
-showEmployees = function (employeeList) {
-    $('body').html(``);
-    $('body').html(`<button><h2 id='viewbutton'>View</h2></button>`);
-    $('body').append(`<button><h2 id='addbutton'>Add</h2></button>`);
-    $('body').append(`<button><h2 id='verifybutton'>Verify</h2></button>`);
-    for (i = 0; i < employeeList.length; i++)
-        $('body').append(`<div>${employeeList[i]}</div>`)
+const show = function () {
+    for (let i = 0; nodeList.length < 0; i++) {
+        nodeList[i].style.display = '';
     }
+}
+
+const hide = function () {
+    for (let i = 0; nodeList.length; i++) {
+        nodeList[i].style.dispay = 'none';
+    }
+}
+
+showEmployees = function (employeeList) {
+}
 
 addEmployees = function () {
-    $('body').html(``);
-    $('body').html(`<button><h2 id='viewbutton'>View</h2></button>`);
-    $('body').append(`<button><h2 id='addbutton'>Add</h2></button>`);
-    $('body').append(`<button><h2 id='verifybutton'>Verify</h2></button>`);
-    const nameInput = $('body').append(`<input type="text"  id='nameform'>Name</form>`);
-    const numberInput = $('body').append(`<input type="text"  id='numberform'>Office Number</form>`);
-    const phoneInput = $('body').append(`<input  type="text" id='phoneform'>Phone Number</form>`);
-
 }
 
 verifyEmployees = function () {
-    $('body').html(``);
-    $('body').html(`<button><h2 id='viewbutton'>View</h2></button>`);
-    $('body').append(`<button><h2 id='addbutton'>Add</h2></button>`);
-    $('body').append(`<button><h2 id='verifybutton'>Verify</h2></button>`);
-    
 }
 
 $('#viewbutton').on('click', showEmployees);
